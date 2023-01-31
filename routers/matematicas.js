8f3cf60d917d84064c57d950952bd4b4991356f4
Lautaro Acosta <soylautaroacosta@gmail.com>
2023-01-31 00:45:04 -0300

    update matematica

--- routers/matematicas.js
+++ routers/matematicas.js
@@ -4,6 +4,15 @@ const {matematicas} = require('../cursos/cursos.js').infoCursos
 const routerMatematica = express.Router()
 
 
+// middleware
+
+routerMatematica.use(express.json())
+
+
+
+routerMatematica.get('/', (req, res) => {
+    res.send(JSON.stringify(matematicas))
+})
 
 routerMatematica.get('/:materia', (req, res) => {
     // capturo el valor de la url
@@ -17,5 +26,83 @@ routerMatematica.get('/:materia', (req, res) => {
 })
 
 
+// mas de un filtro
+
+routerMatematica.get('/:materia/:vistas', (req, res) => {
+    const materia = req.params.materia
+    const vistas = req.params.vistas
+
+    const resultado = matematicas.filter(tema => tema.titulo === materia && tema.vistas === vistas)
+
+    if (resultado.length === 0){
+        res.status(404).send(`No tenemos esa materia`)
+    }
+
+    res.send(JSON.stringify(resultado))
+})
+
+
+
+
+// post
+routerMatematica.post('/', (req, res) => {
+    let materiaNueva = req.body;
+
+    matematicas.push(materiaNueva)
+    req.send(JSON.stringify(matematicas))
+})
+
+
+
+// put
+routerMatematica.put('/:id', (req, res) => {
+    const cursoActualizado = req.body;
+    const id = req.params.id
+
+    // busco el id
+    const indice = matematicas.findIndex(curso => curso.id == id)
+
+
+    if(indice >= 0){
+        matematicas[indice] = cursoActualizado;
+    }
+    res.send(JSON.stringify(matematicas))
+
+})
+
+
+// patch
+routerMatematica.patch('/:id', (req, res) => {
+    const infoActualizada = req.body;
+
+    const id = req.params.id
+
+    const indice = matematicas.findIndex(curso => curso.id == id)
+
+    if(indice >= 0){
+        const cursoModificar = matematicas[indice]
+        
+        // modifico los datos
+        Object.assign(cursoModificar, infoActualizada)
+        
+    }
+
+    res.send(JSON.stringify(matematicas))
+})
+
+
+
+// delete
+routerMatematica.delete('/:id', (req, res) => {
+    const id = req.params.id
+
+    const indice = matematicas.findIndex(curso => curso.id == id)
+
+
+    if(indice >= 0){
+        matematicas.splice(indice, 1)
+    }
+    res.send(JSON.stringify(matematicas))
+})
 
 module.exports = routerMatematica
